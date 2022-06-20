07defca932f052515cf0a43149e12b45640eb3cc
lazyspell <Jeremy2975@yahoo.com>
2022-06-19 23:57:04 -0700

    code clean up

--- models/users.go
+++ models/users.go
@@ -35,7 +35,6 @@ type GoogleObject struct {
 	Locale    string
 }
 
-//TODO: need to remove the Password frield from teh User model and switch everything to the password model
 type Password struct {
 	Password string `json:"password"`
 }
